d817c159d6d955a4f169b30dae4ab8d6f2dc37d2
ngolkim <110917453+ngolkim@users.noreply.github.com>
2023-10-15 16:24:30 +0700

    update 1

--- routes/Users/UserController.js
+++ routes/Users/UserController.js
@@ -5,8 +5,8 @@ const login = async (email, password , sdt) => {
     return await userService.login(email, password, sdt);
 }
 
-const register = async (email, password, name, sdt) => {
-    return await userService.register(email, password, name, sdt);
+const register = async (name, email,sdt ,password ) => {
+    return await userService.register(name, email,sdt ,password );
 }
 
 module.exports = { login, register};
